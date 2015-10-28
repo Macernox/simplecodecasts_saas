02d5ba28960a785dd20eb12f64271986bdba3fe7
Cam Mackenzie <melodiousnocturne@live.ca>
2015-10-28 03:59:53 +0000

    Added action mailer settings

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,6 +5,6 @@ class ContactMailer < ActionMailer::Base
         @email = email
         @body = body
         
-        mail(form: email, subject: 'Contact Form Message')
+        mail(from: email, subject: 'Contact Form Message')
     end
 end
