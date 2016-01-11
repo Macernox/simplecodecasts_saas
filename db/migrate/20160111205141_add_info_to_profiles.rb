55a75031916b822931eb22b891a31c1e4ce80f23
Cam Mackenzie <melodiousnocturne@live.ca>
2016-01-11 20:58:25 +0000

    added profile model and table

---
+++ db/migrate/20160111205141_add_info_to_profiles.rb
@@ -0,0 +1,13 @@
+class AddInfoToProfiles < ActiveRecord::Migration
+  def change
+    add_column :profiles, :plan_id, :integer
+    add_column :profiles, :first_name, :string
+    add_column :profiles, :last_name, :string
+    add_column :profiles, :job_title, :string
+    add_column :profiles, :phone_number, :string
+    add_column :profiles, :contact_email, :string
+    add_column :profiles, :description, :text
+    add_column :profiles, :created_at, :datetime
+    add_column :profiles, :updated_at, :datetime
+  end
+end
